ae60cf26419252172634cb9952f1781a0a8f02fd
Guido Marchini <guido.marchini@gmail.com>
2021-08-08 12:00:28 -0300

    #1 initial project structure using springboot

---
+++ src/main/kotlin/com/bol/gmarchini/kalaha/KalahaApplication.kt
@@ -0,0 +1,11 @@
+package com.bol.gmarchini.kalaha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KalahaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KalahaApplication>(*args)
+}
