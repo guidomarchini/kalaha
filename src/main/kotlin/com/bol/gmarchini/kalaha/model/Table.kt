316f55c21268ceea4e900f5500f3457ace9228f6
Guido Marchini <guido.marchini@gmail.com>
2021-08-15 22:57:58 -0300

    #11 made table vals public to see them in api response

--- src/main/kotlin/com/bol/gmarchini/kalaha/model/Table.kt
+++ src/main/kotlin/com/bol/gmarchini/kalaha/model/Table.kt
@@ -8,10 +8,10 @@ package com.bol.gmarchini.kalaha.model
  * Each pit can have any amount of stones.
  */
 data class Table (
-    private val southernPits: MutableList<Int>,
-    private val northernPits: MutableList<Int>,
-    private var southernKalaha: Int,
-    private var northernKalaha: Int
+    val southernPits: MutableList<Int>,
+    val northernPits: MutableList<Int>,
+    var southernKalaha: Int,
+    var northernKalaha: Int
 ) {
     fun getPits(side: Side): MutableList<Int> = when(side) {
         Side.SOUTH -> southernPits
